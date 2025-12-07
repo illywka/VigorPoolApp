16aa09d4ec05644e95069fa92350621729632ea0
ilywka_ <104511359+illywka@users.noreply.github.com>
2025-12-07 16:26:57 +0200

    Reworked 1.3

--- VigorPoolApp.py
+++ VigorPoolApp.py
@@ -7,7 +7,7 @@ import requests
 import threading
 
 st.set_page_config(page_title="VigorMonitor Ultimate", page_icon="⚡", layout="centered")
-
+global display_time
 # --- 1. СПІЛЬНА ПАМ'ЯТЬ ---
 @st.cache_resource
 class SharedStorage:
@@ -173,13 +173,11 @@ def worker_telegram():
                         if "/status" in text or "статус" in text or "start" in text:
                             s = storage.data
                             upd = time.strftime("%H:%M:%S", time.localtime(storage.last_update))
-                            h, m = s['time_left'] // 3600, (s['time_left'] % 3600) // 60
-                            time = f"{h}г {m:02d}хв"
                             reply = (
                                 f"Батарея: {s['battery']}%\n\n"
                                 f"🟢 Вхід: {s['in_watts']} W\n"
                                 f"🔌 Вихід: {s['out_watts']} W\n"
-                                f"Часу залишилось: {time}\n\n"
+                                f"Часу залишилось: {display_time}\n\n"
                                 f"Оновлено о {upd}"
                             )
                             send_telegram_bg(reply, target_id=cid)
